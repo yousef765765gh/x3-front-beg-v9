7865c7a007e43d88d0cae41ecbf3b8680a4b20b8
YousefBrro <ybrro2003@gmail.com>
2026-01-18 20:40:33 +0300

    our services and choose squareUp

---
+++ src/components/OurServices/OurServices.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import "./OurServices.css"
+
+const OurServices = ({icon , title , description , showButton = true , horizontal = false}) => {
+    return (
+        <div className="yb-card">
+            <div className={`yb-container ${horizontal ? "horizontal" : ""}`}>
+                <div className="yb-icon">
+                <img className="yb-img" src={icon} />
+                </div>
+                <h1 className="yb-title">{title}</h1>
+            </div>
+            <p className="yb-description">{description}</p>
+            {showButton && (
+                <div className="yb-btn">
+                    <Link to="/Work" className="yb-link">{showButton}</Link>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default OurServices
