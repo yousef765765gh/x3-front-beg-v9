c3946652b878f2af57cad7f0b020ee29ef504221
yousefghanem <yousef765765gh@gmail.com>
2026-02-03 22:55:40 +0300

    work-x3

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import AboutDashboard from "../components/AboutDashboard/AboutDashboard"
+import DashBordSecWork from "../components/DashBordSecWork/DashBordSecWork"
 import DASliderDashboard from "../components/DASliderDashboard/DASliderDashboard"
 import ProcessDashboard from "../components/ProcessDashboard/ProcessDashboard"
-import DashBordSecWork from "../components/DashBordSecWork/DashBordSecWork"
 const Dashboard = () => {
         return (
         <>
