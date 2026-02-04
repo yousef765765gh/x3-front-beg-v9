629eb1eee8670c075350b7f440cc4d166c555af5
yousefghanem <yousef765765gh@gmail.com>
2026-02-04 19:15:54 +0300

    fx-x3

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import { BrowserRouter } from 'react-router-dom'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <BrowserRouter basename='/x3-front-beg-v9/'>
-      <App />
-    </BrowserRouter> */}
     <HashRouter>
       <App />
     </HashRouter>
